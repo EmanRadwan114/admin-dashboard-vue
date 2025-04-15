eac903018b34419e6e2bed77445f9cfbdd4d0d0a
Eman Radwan <emanalaa11499@gmail.com>
2025-04-15 19:53:28 +0200

    deploy on vercel

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/admin-dashboard-vue/", // 🔁 change this to your GitHub repo name
+  publicPath: "/", // 🔁 change this to your GitHub repo name
 });
